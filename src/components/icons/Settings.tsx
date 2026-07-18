67965cfa3c96633adf1c63c99825f1769e4511be
Harrison Deo <github@harrisondeo.me.uk>
2026-07-19 00:14:44 +0100

    fix icon fill for lightmode

--- src/components/icons/Settings.tsx
+++ src/components/icons/Settings.tsx
@@ -6,7 +6,7 @@ const Settings = (props: SVGProps<SVGSVGElement>) => {
   return (
     <svg
       viewBox="0 0 24 24"
-      fill="#ffffff"
+      fill="currentColor"
       strokeWidth="2"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
--- src/components/icons/Sync.tsx
+++ src/components/icons/Sync.tsx
@@ -1,7 +1,7 @@
 import type { SVGProps } from "react";
 
 const Sync = (props: SVGProps<SVGSVGElement>) => (
-  <svg viewBox="0 0 24 24" fill="#e8eaed" xmlns="http://www.w3.org/2000/svg" {...props}>
+  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path d="M12 4V1L8 5l4 4V6c3.31 0 6 2.69 6 6 0 1.01-.25 1.97-.7 2.8l1.46 1.46C19.54 15.03 20 13.57 20 12c0-4.42-3.58-8-8-8zm0 14c-3.31 0-6-2.69-6-6 0-1.01.25-1.97.7-2.8L5.24 7.74C4.46 8.97 4 10.43 4 12c0 4.42 3.58 8 8 8v3l4-4-4-4v3z" />
   </svg>
 );
